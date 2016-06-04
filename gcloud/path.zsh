e0668d0b9482c8a2b2a06972719b08d75e5fb93f
Marc Chung <mchung@gmail.com>
2016-06-03 21:57:36 -0700

    Adding in gcloud

---
+++ gcloud/path.zsh
@@ -0,0 +1,7 @@
+export GOOGLE_CLOUD=$HOME/sdk/google-cloud-sdk
+
+# The next line updates PATH for the Google Cloud SDK.
+source $GOOGLE_CLOUD/path.zsh.inc
+
+# The next line enables shell command completion for gcloud.
+source $GOOGLE_CLOUD/completion.zsh.inc
