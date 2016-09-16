3795080c1d5d1ee4681e54a0803679a37369c718
Marc Chung <marc@joinhandshake.com>
2016-09-16 08:46:03 -0700

    Setup rbenv with a gem folder

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,5 +1,12 @@
 # init according to man page
 if (( $+commands[rbenv] ))
 then
+  export RBENV_ROOT=$HOME/.rbenv
+  export PATH="$RBENV_ROOT/bin:$PATH"
   eval "$(rbenv init -)"
+
+  # Allow local Gem management
+  export GEM_HOME="$HOME/.gem"
+  export GEM_PATH="$HOME/.gem"
+  export PATH="$HOME/.gem/bin:$PATH"
 fi
