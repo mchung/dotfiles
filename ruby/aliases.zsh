c7550ca881fc5c046de71fecb7e3c9bb519fd2e6
Marc Chung <marc@joinhandshake.com>
2016-10-03 10:33:49 -0700

    Some Ruby helpers

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,26 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+# Rails gives me carpal
+alias app-go='foreman start -f Procfile.dev'
+alias app-run='foreman run $@ -f Procfile.dev'
+alias app-exec='foreman run -f Procfile.dev bundle exec $@'
+alias app-console='app-exec rails console'
+app-grep() {
+  ag "$@" `bundle show --paths`
+}
+
+app-find() {
+  find `bundle show --paths` -name "$@"
+}
+
+app-help() {
+  alias app-go
+  alias app-run
+  alias app-exec
+  which app-grep
+  which app-find
+}
+
+# Various search helpers on bundled gems
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,3 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
-
-rgrems() {
-  ag "$@" `bundle show --paths`
-}
