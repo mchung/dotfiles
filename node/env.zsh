d90710961e3c89fe85399464d70330eaf1a5d7bd
Marc Chung <marc@joinhandshake.com>
2016-12-29 08:12:34 -0800

    Use nvm if it exists

--- node/env.zsh
+++ node/env.zsh
@@ -1,5 +1,10 @@
 export NODE_PATH="$(npm root -g)" # Node can now reference global packages
 
+if [[ -a /usr/local/opt/nvm/nvm.sh ]]; then
+  export NVM_DIR="$HOME/.nvm"
+  . "/usr/local/opt/nvm/nvm.sh"
+fi
+
 if [[ -a /usr/local/bin/n ]]; then
   n 0.12.1
 fi
