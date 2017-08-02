e44e30c0e04ea754de34513a4f0c9c6490d2ff70
Marc Chung <marc@joinhandshake.com>
2017-08-02 16:04:18 -0700

    Updates

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,11 +1,7 @@
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
 # Rails gives me carpal
-alias app-go='foreman start -f Procfile.dev'
-alias app-run='foreman run $@ -f Procfile.dev'
-alias app-exec='foreman run -f Procfile.dev bundle exec $@'
+alias app-go='foreman start -f Procfile.dev' # start the dev env
+alias app-run='foreman run $@ -f Procfile.dev' # 'app-run web' starts the web process
+alias app-exec='foreman run bundle exec $@' # 'app-exec cmd args' loads entire env
 alias app-console='app-exec rails console'
 
 # Various search helpers on bundled gems
@@ -24,6 +20,13 @@ function app-help() {
   alias app-exec
   which app-grep
   which app-find
+  which app-kill
+}
+
+function app-kill() {
+  echo "Sending SIGKILL to node/puma processes"
+  ps aux | grep 'node' | grep 'hot' | awk '{print $2}' | xargs kill -9
+  ps aux | grep 'puma' | grep 'handshake' | awk '{print $2}' | xargs kill -9
 }
 
 function up() {
@@ -35,6 +38,7 @@ function up() {
 
 # $ heroku_console heroku-slug-name
 function heroku_console() {
-  echo "Connecting to $@ on FOLLOWER_DATABASE_URL"
-  heroku run DATABASE_URL=`heroku config:get FOLLOWER_DATABASE_URL -a $@` rails console -a $@
+  echo "Connecting to $@ on first KNOCKOFF_REPLICA_ENVS url"
+  first_follower=`heroku config:get KNOCKOFF_REPLICA_ENVS -a $@ | cut -f 1 -d ","`
+  heroku run DATABASE_URL=`heroku config:get $first_follower -a $@` rails console -a $@
 }
