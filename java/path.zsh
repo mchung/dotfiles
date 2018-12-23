f030d61b605a0a768db2b4014474ca9772cb1a25
Marc Chung <marc@joinhandshake.com>
2018-12-23 10:25:09 -0800

    JENV

--- java/path.zsh
+++ java/path.zsh
@@ -1,1 +1,5 @@
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8.0`
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+jenv global oracle64-1.8.0.181
+# export JAVA_HOME=`jenv javahome`
+# export JAVA_HOME=`/usr/libexec/java_home -v 10.0.2`
