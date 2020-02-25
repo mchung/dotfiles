17818e9a3fe09bd7b39e953298052d25db2200c8
mchung <marc@joinhandshake.com>
2020-02-24 17:02:22 -0800

    Conditions

--- java/path.zsh
+++ java/path.zsh
@@ -1,5 +1,7 @@
-export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
-jenv global oracle64-1.8.0.181
-# export JAVA_HOME=`jenv javahome`
-# export JAVA_HOME=`/usr/libexec/java_home -v 10.0.2`
+if [[ -a /usr/local/bin/jvm ]]; then
+  export PATH="$HOME/.jenv/bin:$PATH"
+  eval "$(jenv init -)"
+  jenv global oracle64-1.8.0.181
+  # export JAVA_HOME=`jenv javahome`
+  # export JAVA_HOME=`/usr/libexec/java_home -v 10.0.2`
+fi
